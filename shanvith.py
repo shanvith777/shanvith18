b2a2fe470ac88531e5026cad939eb6fd1abdcc10
shanvith777 <shanvithch018@gmail.com>
2026-08-31 13:27:43 +0530

    shanvith.py

---
+++ shanvith.py
@@ -0,0 +1,7 @@
+num1 =float(input("enter the first number"))
+num2 =float(input("enter the second number"))
+num3 =float(input("enter the third number"))
+
+total = num1 + num2 + num3
+
+print("the sum of the three numbers is :", total)
