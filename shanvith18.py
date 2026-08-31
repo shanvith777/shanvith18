1b38e6136c2d09baff342095cdd7a773de421f38
shanvith777 <shanvithch018@gmail.com>
2026-08-31 14:23:44 +0530

    shanvith18.py

---
+++ shanvith18.py
@@ -0,0 +1,8 @@
+a=int(input("enter the first number "))
+b=int(input("enter the secong number"))
+print("addition=",a+b)
+print("subtraction=",a-b)
+print("multiplication=",a*b)
+print("division=",a/b)
+print("floor division=",a/b)
+      
