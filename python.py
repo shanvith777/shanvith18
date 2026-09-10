732bae1469fa45915887f5a6618d511b48f71732
shanvith777 <shanvithch018@gmail.com>
2026-09-10 15:13:11 +0530

    6

--- python.py
+++ python.py
@@ -1,2 +1,13 @@
-print("hello sir")
-print("welcome to the Python Programming")
+a = float(input("Enter first number: "))    
+b = float(input("Enter second number: "))
+
+print =("addition", a + b)
+print =("subtraction", a - b)
+print =("multiplication", a * b)
+if b != 0:
+    print =("division", a / b)  
+    print =("modulus", a % b)
+    print =("floor division", a // b)   
+    else:
+    print("division,modules and floor division are not possible as the second number is zero.")
+    print("exponentiation", a ** b)
