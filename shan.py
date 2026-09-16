88f746d9f7bee7c591b5eae82aeaf58f5f3c50d0
shanvith777 <shanvithch018@gmail.com>
2026-09-16 08:59:48 +0530

    b

---
+++ shan.py
@@ -0,0 +1,8 @@
+total_classes = int(input("Enter the total number of classes conducted: "))
+classes_attended = int(input("Enter the number of classes attended: "))
+
+if total_classes > 0:
+	attendance_percentage = (classes_attended / total_classes) * 100
+	print(f"Attendance percentage: {attendance_percentage:.2f}%")
+else:
+	print("Total classes must be greater than zero.")
