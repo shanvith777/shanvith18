1d603cb17b153faff5ebd0790b804cbc8d87b442
shanvith777 <shanvithch018@gmail.com>
2026-09-24 14:20:51 +0530

    lab 8

---
+++ lab 8 (Q 1).py
@@ -0,0 +1,9 @@
+name=input("Enter students name")
+rollnumber = input("enter roll number")
+course = input("enter course")
+marks =input("total marks")
+
+print(f"student name : {name}")
+print(f"roll number  : {rollnumber}")
+print(f"course       : {course}")
+print(f"marks        : {marks}")
---
+++ lab 8 (Q. 2).py
@@ -0,0 +1,8 @@
+name=input("Enter students name")
+rollnumber = input("enter roll number")
+course = input("enter course")
+marks =input("total marks")
+
+print("student name : {}".format(name))
+print("roll number  : {}".format(rollnumber))
+print("course       : {}".format(course))
